c4a5d9a4c1dd10349a843c255344ab15ccb2ef12
demavros <hategekimanahirwaarnold@gmail.com>
2025-12-29 12:41:06 +0200

    Add version for 2025 students

--- channel_id.py
+++ channel_id.py
@@ -5,12 +5,13 @@ import os
 
 
 load_dotenv()
-token=os.environ['MONKEYS']
+token=os.environ['MONKEYS_TOKEN']
 
 client = WebClient(token=token)
 
 try:
-    response = client.conversations_list(types="private_channel")
+    # response = client.conversations_list(types="private_channel")
+    response = client.conversations_list(types="public_channel")
     for channel in response["channels"]:
         print(f"Name: {channel['name']}, ID: {channel['id']}")
 except SlackApiError as e:
